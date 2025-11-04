59fd7cbee8effedff6a19d594d4dbd7115901e1d
ThorsThunder04 <87436831+ThorsThunder04@users.noreply.github.com>
2025-11-04 01:23:28 +0100

    added comments

--- kd_tree.py
+++ kd_tree.py
@@ -2,31 +2,68 @@
 
 class Node:
     
-    def __init__(self, val: tuple[int,...], dim: int, left: "Node" = None, right: "Node" = None):
+    def __init__(self, val: list[int], dim: int, left: "Node" = None, right: "Node" = None):
         self.val = val
         self.dim = dim
         self.left = left
         self.right = right
 
-def build_kd_tree(dim: int, data: list[tuple], _depth: int = 0) -> (Node|None):
+def build_kd_tree(k: int, data: list[list[int]], _depth: int = 0) -> (Node|None):
+    """Build a kd-tree for a dataset of points in the given dimension
+    
+
+    Parameters
+    ----------
+    k : int
+        The dimension of the kd-tree we want to build
+    data : list[list[int]]
+        The list of `k` dimensional points we want to insert into the tree
+       
+
+    Returns
+    -------
+    Node|None
+        The root of the build kd-tree (None if `data` is empty) 
+    """
     
     if len(data) == 0:
         return None
     if len(data) == 1:
-        return Node(data[0], dim, None, None)
+        return Node(data[0], k, None, None)
     
-    axis = _depth % dim
+    # Calculate the axis used at this depth
+    axis = _depth % k
     median = len(data) // 2
 
+    # we sort all the data to have a balanced subtree
     data.sort(key = lambda x: x[axis])
 
-    curr_node = Node(data[median], dim)
+    # create node for the median point
+    curr_node = Node(data[median], k)
 
-    curr_node.left = build_kd_tree(dim, data[:median], _depth + 1)
-    curr_node.right = build_kd_tree(dim, data[median+1:], _depth + 1)
+    # repeat process for both child subtrees
+    curr_node.left = build_kd_tree(k, data[:median], _depth + 1)
+    curr_node.right = build_kd_tree(k, data[median+1:], _depth + 1)
 
     return curr_node
 
+def find_NN(point: tuple[int,...], tree: Node) -> tuple[int,...] :
+    """Find the nearest neighbor in the kd-tree to the given point
+
+    Parameters
+    ----------
+    point : tuple[int,...]
+        The reference point to which we want to find the nearest neighbor for
+    tree : Node
+        kd-tree containing point we are searching for
+    
+    
+    Returns
+    -------
+    tuple[int,...]
+        The nearest point in `tree` to `point`
+    """
+    pass 
 
 
 if __name__ == "__main__":
