52300ff4cecbc7758ed23a708ba8afdcadd56539
ThorsThunder04 <87436831+ThorsThunder04@users.noreply.github.com>
2025-11-04 16:39:53 +0100

    implementation of nearest neighbor search with K dimensions (not 100% equal to linear serach yet)

--- kd_tree.py
+++ kd_tree.py
@@ -2,13 +2,13 @@
 
 class Node:
     
-    def __init__(self, val: list[int], dim: int, left: "Node" = None, right: "Node" = None):
+    def __init__(self, val: list[float], dim: int, left: "Node" = None, right: "Node" = None):
         self.val = val
         self.dim = dim
         self.left = left
         self.right = right
 
-def build_kd_tree(k: int, data: list[list[int]], _depth: int = 0) -> (Node|None):
+def build_kd_tree(k: int, data: list[list[float]], _depth: int = 0) -> (Node|None):
     """Build a kd-tree for a dataset of points in the given dimension
     
 
@@ -16,7 +16,7 @@ def build_kd_tree(k: int, data: list[list[int]], _depth: int = 0) -> (Node|None)
     ----------
     k : int
         The dimension of the kd-tree we want to build
-    data : list[list[int]]
+    data : list[list[float]]
         The list of `k` dimensional points we want to insert into the tree
        
 
@@ -47,12 +47,18 @@ def build_kd_tree(k: int, data: list[list[int]], _depth: int = 0) -> (Node|None)
 
     return curr_node
 
-def find_NN(point: tuple[int,...], tree: Node) -> tuple[int,...] :
+def k_dist(pt1: tuple[float,...], pt2: tuple[float,...], k: int) -> float:
+    
+    square_sum = sum((pt1[i] - pt2[i])**2 for i in range(k))
+    return square_sum**0.5
+    
+
+def find_NN(point: tuple[float,...], tree: Node, _mindist: float = -1, _depth: int = 0) -> tuple[float,...] :
     """Find the nearest neighbor in the kd-tree to the given point
 
     Parameters
     ----------
-    point : tuple[int,...]
+    point : tuple[float,...]
         The reference point to which we want to find the nearest neighbor for
     tree : Node
         kd-tree containing point we are searching for
@@ -63,18 +69,83 @@ def find_NN(point: tuple[int,...], tree: Node) -> tuple[int,...] :
     tuple[int,...]
         The nearest point in `tree` to `point`
     """
-    pass 
-
+    
+    minpt = None
+    # basically just rejects this case
+    if tree is None:
+        return (minpt, _mindist + 1) # if it returns a bigger minimum, it's never gonna be good
+    
+    # set an initial distance to use in future comparisons
+    if _mindist == -1:
+        _mindist = k_dist(point, tree.val, tree.dim)
+        minpt = tree.val
+    
+    axis = _depth % tree.dim
+    went_left = True # used to know which branch to go down in case there is possible nearer point down other branch
+    
+    
+    # go through tree until a leaf is reached
+    if point[axis] < tree.val[axis] and tree.left is not None:
+        minpt, this_mindist = find_NN(point, tree.left, _mindist, _depth + 1)
+        if this_mindist < _mindist: _mindist = this_mindist
+    elif point[axis] > tree.val[axis] and tree.right is not None:
+        went_left = False
+        minpt, this_mindist = find_NN(point, tree.right, _mindist, _depth + 1)
+        if this_mindist < _mindist: _mindist = this_mindist
+    
+    
+    # calc if the current point is also the current nearest
+    d = k_dist(tree.val, point, tree.dim)
+    if d < _mindist:
+        minpt = tree.val
+        _mindist = d
+    
+    # if possible nearer point in other branch, do a NN search down that branch
+    if abs(point[axis] - tree.val[axis]) < _mindist:
+        other_minpt, other_mindist = find_NN(point, tree.right if went_left else tree.left, _mindist, _depth + 1)   
+        if other_mindist < _mindist:
+            _mindist = other_mindist
+            minpt = other_minpt
+    
+    return (minpt, _mindist)
+            
 
 if __name__ == "__main__":
     import random as r
     
-    SEED = 123
-    K = 3
-    SIZE = 10
-    bounds = (0, 256)
-
+    SEED = None
     r.seed(SEED)
-    dataset = [[r.randint(*bounds) for _ in range(K)] for _ in range(SIZE)]
+    K = 3
+    SIZE = 10000 
+    bounds = (0, 2560)
+
+    def linear_closest(point: tuple[float,...], points: list[list[float]], k: int) -> tuple[tuple[float,...], float]:
+        
+        curr_best = (points[0], k_dist(point, points[0], K))
+
+        for pt in points:
+            d = k_dist(point, pt, k)
+            if d < curr_best[1]:
+                curr_best = (pt, d)
+        
+        return curr_best
+
+    def gen_n_rand_pts(n, dim, bounds):
+        return [[r.randint(*bounds) for _ in range(dim)] for _ in range(n)]
+    
+    
+
+    dataset = gen_n_rand_pts(SIZE, K, bounds)
     
     tree = build_kd_tree(K, dataset)
+
+    for _ in range(50):
+        test_pt = gen_n_rand_pts(1, K, bounds)[0]
+        res1 = find_NN(test_pt, tree) 
+        res2 = linear_closest(test_pt, dataset, K)
+        print(res2)
+        print(res1)
+        assert res1 == res2
+        print()
+
+    
